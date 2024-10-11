c2345c5a6f42bdf0b418b88b27827dcbe528a8ea
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-10-11 10:09:46 +0900

    import 修正

--- src/app/(pages)/main/page.tsx
+++ src/app/(pages)/main/page.tsx
@@ -13,7 +13,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { getAllTogos } from "@/lib/supabase/supabaseFunctions";
 import { Database } from "@/types/supabase";
-import Header from "@/app/features/components/header";
+import Header from "@/app/features/components/Header";
 
 type Togo = Database['public']['Tables']['togo']['Row'];
 
