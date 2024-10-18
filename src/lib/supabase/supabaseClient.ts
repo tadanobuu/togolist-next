3c9e312b76e95d82fb8821a73bea49a3910b9218
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-10-19 01:06:27 +0900

    テスト用 console.logの追加

--- src/lib/supabase/supabaseClient.ts
+++ src/lib/supabase/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '../../types/supabase'
 
+console.log("Supabase URL: ", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL!;
 const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY!;
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
