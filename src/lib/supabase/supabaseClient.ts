0f61bc75465c7f93d686391b72522d2781c31931
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-10-19 00:37:28 +0900

    環境変数の変更

--- src/lib/supabase/supabaseClient.ts
+++ src/lib/supabase/supabaseClient.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from '../../types/supabase'
 
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY!
+const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
